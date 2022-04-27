ee840286b927de002dfcc41c8c6ef95150e12d11
Jesse Newland <jesse@jnewland.com>
2022-04-27 16:24:59 +0000

    matching

--- renovate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  logLevel: 'debug',
-  onboarding: true,
-  requireConfig: false,
-  platform: 'github',
-  printConfig: true,
-  gitAuthor: 'ur bot <72803780+urcomputeringpal-bot@users.noreply.github.com>',
-  repositories: []
-}
