a5cbe525b70d46f67892f9fc3f5c4c15ffde1474
Jesse Newland <jesse@jnewland.com>
2022-04-27 16:08:41 +0000

    add renovate

---
+++ renovate.js
@@ -0,0 +1,8 @@
+module.exports = {
+  logLevel: 'debug',
+  onboarding: true,
+  requireConfig: false,
+  printConfig: true,
+  gitAuthor: 'ur bot <72803780+urcomputeringpal-bot@users.noreply.github.com>',
+  repositories: []
+}
