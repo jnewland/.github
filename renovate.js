a025c0bfb86968e637279c341d591178e659bbe9
Jesse Newland <jesse@jnewland.com>
2023-01-03 10:26:17 -0500

    set token, username, author here

--- renovate.js
+++ renovate.js
@@ -3,18 +3,10 @@ module.exports = {
   "logLevel": "debug",
   "onboarding": true,
   "requireConfig": false,
-  "platform": "github",
   "printConfig": true,
-  "username": "jnewland-renovate[bot]",
-  "gitAuthor": "jnewland-bot <195354+jnewland-renovate[bot]@users.noreply.github.com>",
   "repositories": [],
   "allowedPostUpgradeCommands": [
     "./script/sync-components",
     "make"
   ],
-  "hostRules": [{
-    "matchHost": "api.github.com",
-    "token": process.env.RENOVATE_GITHUB_COM_TOKEN,
-    "username": "jnewland-renovate[bot]"
-  }]
 }
